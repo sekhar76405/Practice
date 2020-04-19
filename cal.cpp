6e60aa6b989b75208995c55745cea8c312c91499
Yashvishnoi <jivishnoi@gmail.com>
2020-04-19 22:51:21 +0530

    Trying to make calculator

---
+++ cal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int add(int x, int y)
+{
+int z=
+return z;
+}
+
+int main()
+{
+int a,b;
+cin>>a>>b;
+int c=add(a,b);
+cout<<"Addition of two number is "<<c<<endl;
+return 0;
+}
